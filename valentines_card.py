999362422f8989064ab7a0003866074c737305e4
melihakoc <melihakoc17@gmail.com>
2025-02-05 22:58:50 +0300

    Initial commit

---
+++ valentines_card.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Title and Header
+st.title("♠️ You are sleeping and I'm bored ♥️")
+st.header("so...")
+
+# Message
+st.write("just wanted to say **good game** at life. You're a solid player.")
+st.write("Jokes aside, you're awesome and I'm glad you're around.💕")
+
+# Fun Interaction
+if st.button("Draw a Card 🎴"):
+    st.write("You pulled the Ace of Hearts. Take that as a sign. 😉")
+
+# Footer
+st.write("*So what?* 🤷‍♀️")
